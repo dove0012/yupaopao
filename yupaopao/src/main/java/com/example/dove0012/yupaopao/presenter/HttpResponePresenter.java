dccaf74f15c4aa4163aaf048bbbf39a71f2dc1af
liuzl <dove0012@163.com>
2016-07-28 11:18:51 +0800

    优化presenter，解决rxjava解除绑定，避免内存溢出问题

--- yupaopao/src/main/java/com/example/dove0012/yupaopao/presenter/HttpResponePresenter.java
+++ yupaopao/src/main/java/com/example/dove0012/yupaopao/presenter/HttpResponePresenter.java
@@ -13,32 +13,18 @@ import rx.schedulers.Schedulers;
  * Created by liuzl on 2016/3/25.
  */
 public abstract class HttpResponePresenter {
-    private boolean loading = false;
     private HttpResponeBean httpResponeBean;
-    private long createTime;
     private httpSubscriber subscriber = null;
 
     public void getRespone(String url, httpSubscriber<HttpResponeBean> subscriber) {
-        getRespone(url, null, 6000, subscriber);
+        getRespone(url, null, subscriber);
     }
 
     public void getRespone(String url, RequestParams params, httpSubscriber<HttpResponeBean> subscriber) {
-        getRespone(url, params, 6000, subscriber);
-    }
-
-    public void getRespone(String url, RequestParams params, long cacheTime, httpSubscriber<HttpResponeBean> subscriber) {
+        //要是之前已经订阅事件,先取消之前的订阅
+        unsubscribe();
         this.subscriber = subscriber;
-        if (loading) {
-            ToastUtils.show("数据加载中!");
-            return;
-        }
-        //判断缓存
-        long time = System.currentTimeMillis() - createTime;
-        if (httpResponeBean != null && time < cacheTime) {
-            subscriber.mOnNext(httpResponeBean);
-            return;
-        }
-        loading = true;
+
         HttpResponeModel.getInstance().getRespone(url, params)
                 .subscribeOn(Schedulers.io())                                 // 在非UI线程中执行getRespone
                 .observeOn(AndroidSchedulers.mainThread())                   // 在UI线程中执行结果
@@ -46,21 +32,20 @@ public abstract class HttpResponePresenter {
     }
 
     public void unsubscribe(){
-        LogUtils.i("------------toUnsubscribe");
-        if (subscriber != null && subscriber.isUnsubscribed())
+        if (subscriber != null && subscriber.isUnsubscribed()) {
+            LogUtils.i("------------toUnsubscribe");
             subscriber.unsubscribe();
+        }
     }
 
     class httpSubscriber<HttpResponeBean> extends Subscriber{
         @Override
         public void onCompleted() {
-            loading = false;
             mOnCompleted();
         }
 
         @Override
         public void onError(Throwable e) {
-            loading = false;
             ToastUtils.show(e.getMessage());
         }
 
@@ -69,7 +54,6 @@ public abstract class HttpResponePresenter {
             com.example.dove0012.yupaopao.bean.HttpResponeBean respone = (com.example.dove0012.yupaopao.bean.HttpResponeBean)o;
             if (1 == respone.getStatus()) {
                 httpResponeBean = respone;
-                createTime = System.currentTimeMillis();
                 mOnNext(respone);
             } else {
                 ToastUtils.show(respone.getInfo());
